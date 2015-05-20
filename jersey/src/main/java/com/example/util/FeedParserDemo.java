a1cfe958be05c39e1da008030e854366ce919a1a
Administrator <Administrator@PC-20150416TMJQ>
2015-05-20 17:24:46 +0800

    update feed demo

--- jersey/src/main/java/com/example/util/FeedParserDemo.java
+++ jersey/src/main/java/com/example/util/FeedParserDemo.java
@@ -24,56 +24,66 @@ import com.sun.syndication.io.FeedException;
 import com.sun.syndication.io.SyndFeedInput;
 
 public class FeedParserDemo {
-	
-	/**used apache commons-feedparser.jar [ for rss|atom]]
+
+	/**
+	 * used apache commons-feedparser.jar [ for rss|atom]]
+	 * 
 	 * @param rss
 	 */
-	public static void useFeedparser(String rss){
+	public static void useFeedparser(String rss) {
 		try {
 			FeedParser parser = FeedParserFactory.newFeedParser();
 
 			FeedParserListener listener = new DefaultFeedParserListener() {
 
 				public void onChannel(FeedParserState state, String title,
-						String link, String description) throws FeedParserException {
+						String link, String description)
+						throws FeedParserException {
 
 					System.out.println("Found a new channel: " + title);
-					if(state.current.getChild("pubDate")!=null)
-						System.out.println("Found a new channel publish datetime:" +state.current.getChild("pubDate").getValue());
+					if (state.current.getChild("pubDate") != null)
+						System.out
+								.println("Found a new channel publish datetime:"
+										+ state.current.getChild("pubDate")
+												.getValue());
 				}
 
 				public void onItem(FeedParserState state, String title,
 						String link, String description, String permalink)
 						throws FeedParserException {
 
-					System.out.println(String.format("Found a new published article:[%s]:[%s][%s] [%s]", title,description,permalink,link));
+					System.out.println(String.format(
+							"Found a new published article:[%s]:[%s][%s] [%s]",
+							title, description, permalink, link));
 				}
 
 				public void onCreated(FeedParserState state, Date date)
 						throws FeedParserException {
 					System.out.println("Which was created on: " + date);
 				}
-				
+
 				public void onImage(FeedParserState state, String title,
 						String link, String url) throws FeedParserException {
 					super.onImage(state, title, link, url);
-					System.out.println(String.format("Which was a Image: %s",  url));
+					System.out.println(String.format("Which was a Image: %s",
+							url));
 				}
-				
+
 				public void onIssued(FeedParserState state, String content)
 						throws FeedParserException {
 					super.onIssued(state, content);
-					System.out.println(String.format("Which was a Issue: %s",  content));
+					System.out.println(String.format("Which was a Issue: %s",
+							content));
 				}
 			};
 
-
 			System.out.println("Fetching resource:" + rss);
 
-			ResourceRequest request = ResourceRequestFactory.getResourceRequest(rss);
+			ResourceRequest request = ResourceRequestFactory
+					.getResourceRequest(rss);
 			InputStream is = request.getInputStream();
 			parser.parse(listener, is, rss);
-			
+
 		} catch (NetworkException e) {
 			e.printStackTrace();
 		} catch (FeedParserException e) {
@@ -82,46 +92,54 @@ public class FeedParserDemo {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	/**used rome.jar [ for rss|atom]
+
+	/**
+	 * used rome.jar [ for rss|atom]
+	 * 
 	 * @param rss
 	 */
-	public static void useRome(String rss){
+	public static void useRome(String rss) {
 		try {
-			ResourceRequest request = ResourceRequestFactory.getResourceRequest(rss);
+			ResourceRequest request = ResourceRequestFactory
+					.getResourceRequest(rss);
 			InputStream in = request.getInputStream();
-			
+
 			SyndFeedInput input = new SyndFeedInput();
 			InputSource is = new InputSource(in);
-			
+
 			SyndFeed sf = input.build(is);
-			System.out.println("title: "+sf.getTitle());
-			System.out.println("description: "+sf.getDescription());
-			System.out.println("encoding: "+sf.getEncoding());
-			System.out.println("link: "+sf.getLink());
-			System.out.println("publish date: "+DateFormatUtils.format(sf.getPublishedDate(), "yyyy-MM-dd hh:mm:ss"));
+			System.out.println("title: " + sf.getTitle());
+			System.out.println("description: " + sf.getDescription());
+			System.out.println("encoding: " + sf.getEncoding());
+			System.out.println("link: " + sf.getLink());
 			
-			
-			SyndImage si=sf.getImage();
-			System.out.println("image title: "+si.getTitle());
-			System.out.println("image url: "+si.getUrl());
-			System.out.println("image link: "+si.getLink());
-			
-			Iterator<?>  its =sf.getEntries().iterator();
-			while(its.hasNext()){
-				SyndEntry se =(SyndEntry) its.next();
-				String title  = se.getTitle();
-				String link = se.getLink();
-				String destype=se.getDescription().getType();
-				String desvalue = se.getDescription().getValue();
-				String pubdate = DateFormatUtils.format(se.getPublishedDate(), "yyyy-MM-dd hh:mm:ss");
-				
-				System.out.println(String.format("%s %s %s [%s] %s", pubdate,title,link,destype,desvalue));
+			if (sf.getPublishedDate() != null) {
+				System.out.println("publish date: "
+						+ DateFormatUtils.format(sf.getPublishedDate(),
+								"yyyy-MM-dd hh:mm:ss"));
 			}
-			
+
+			SyndImage si = sf.getImage();
+			if (si != null) {
+				System.out.println("image title: " + si.getTitle());
+				System.out.println("image url: " + si.getUrl());
+				System.out.println("image link: " + si.getLink());
+			}
+			Iterator<?> its = sf.getEntries().iterator();
+			while (its.hasNext()) {
+				SyndEntry se = (SyndEntry) its.next();
+				String title = se.getTitle();
+				String link = se.getLink();
+				String destype = se.getDescription().getType();
+				String desvalue = se.getDescription().getValue();
+				String pubdate = DateFormatUtils.format(se.getPublishedDate(),"yyyy-MM-dd hh:mm:ss");
+
+				System.out.println(String.format("%s %s %s [%s] %s", pubdate,
+						title, link, destype, desvalue));
+			}
+
 			in.close();
-			
+
 		} catch (NetworkException e) {
 			e.printStackTrace();
 		} catch (IllegalArgumentException e) {
@@ -133,9 +151,10 @@ public class FeedParserDemo {
 		}
 	}
 
-	public static void main(String[] args){
-		String rss ="http://www.oschina.net/project/rss";// "http://www.duanwenxue.com/data/rss/1.xml";
-		useFeedparser(rss);
+	public static void main(String[] args) {
+		String rss = "http://go.microsoft.com/fwlink/?LinkId=128474";// "http://www.oschina.net/project/rss";//
+																		// "http://www.duanwenxue.com/data/rss/1.xml";
+//		useFeedparser(rss);
 		useRome(rss);
 
 	}
