97ae8a6bfc18328b33d9ab42b97b6c9c6dd1d43d
Administrator <Administrator@PC-20150416TMJQ>
2015-10-16 17:37:31 +0800

    -

--- jersey/src/main/java/com/example/apps/HelloService.java
+++ jersey/src/main/java/com/example/apps/HelloService.java
@@ -1,16 +1,29 @@
 package com.example.apps;
 
+import java.util.Map;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Cookie;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriInfo;
 
-@Path("/hello")
+@Path("example")
 public class HelloService {
 
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String getIt() {
+	public String getIt(@Context UriInfo ui,@Context HttpHeaders hh) {
+		MultivaluedMap<String, String> queryParams = ui.getQueryParameters();
+		MultivaluedMap<String, String> pathParams = ui.getPathParameters();
+		
+		MultivaluedMap<String, String> headerParams = hh.getRequestHeaders();
+		Map<String,Cookie> cookieParams = hh.getCookies();
+		System.out.println(ui.getBaseUri());
 		return "Hello, Let go! use Application Model Set";
 	}
 }
---
+++ jersey/src/main/java/com/example/apps/MyApplication.java
@@ -0,0 +1,17 @@
+package com.example.apps;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("app")
+public class MyApplication extends Application {
+	@Override
+	public Set<Class<?>> getClasses() {
+		Set<Class<?>> s = new HashSet<Class<?>>();
+		s.add(HelloService.class);
+		return s;
+	}
+}
