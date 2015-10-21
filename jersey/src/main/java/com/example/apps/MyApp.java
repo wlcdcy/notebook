dd17ccfa53de1f92cf3f30e7f6175d71a287e999
Administrator <Administrator@PC-20150416TMJQ>
2015-10-21 09:03:48 +0800

    -

--- jersey/src/main/java/com/example/apps/MyApp.java
+++ jersey/src/main/java/com/example/apps/MyApp.java
@@ -7,7 +7,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("app")
-public class MyApplication extends Application {
+public class MyApp extends Application {
 	@Override
 	public Set<Class<?>> getClasses() {
 		Set<Class<?>> s = new HashSet<Class<?>>();
