cfc1aadfb0105a4bef00ad9bea1be342514a84a5
Administrator <Administrator@PC-20150416TMJQ>
2015-11-02 09:55:22 +0800

    add exception module

---
+++ jersey/src/main/java/com/example/error/AuthorizationError.java
@@ -0,0 +1,32 @@
+package com.example.error;
+
+public class AuthorizationError extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AuthorizationError() {
+		super();
+	}
+
+	public AuthorizationError(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public AuthorizationError(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public AuthorizationError(String message) {
+		super(message);
+	}
+
+	public AuthorizationError(Throwable cause) {
+		super(cause);
+	}
+	
+
+}
