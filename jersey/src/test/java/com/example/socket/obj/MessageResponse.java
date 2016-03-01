b68108104670f31dcd4d041dd1b90d5eccde1a84
Administrator <Administrator@PC-20150416TMJQ>
2016-03-01 18:06:58 +0800

    remove socket model

--- jersey/src/test/java/com/example/socket/obj/MessageResponse.java
+++
@@ -1,8 +0,0 @@
-package com.example.socket.obj;
-
-import javolution.io.Struct;
-
-public class MessageResponse extends Struct{
-	public final Unsigned32 offset = new Unsigned32();
-	public final Enum32<State> state = new Enum32<State>(State.values());
-}
